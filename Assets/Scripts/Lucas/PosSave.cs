722268b14642a2f84f9afc2b3781e65992b02f44
GitMaaaan <chaperonlucas@hotmail.fr>
2022-04-07 21:51:16 +0200

    ca marche

--- Assets/Scripts/Lucas/PosSave.cs
+++ Assets/Scripts/Lucas/PosSave.cs
@@ -7,12 +7,20 @@ public class PosSave : MonoBehaviour
 
     [Header("PosSytstem")]
     [SerializeField] GameObject player;
-    [SerializeField] List<Vector3> posForGhost1 = new List<Vector3>();
-    [SerializeField] List<Vector3> posForGhost2 = new List<Vector3>();
-    [SerializeField] List<Vector3> posRecord = new List<Vector3>();
+    private List<Vector3> posForGhost1 = new List<Vector3>();
+    private List<Vector3> posForGhost2 = new List<Vector3>();
+    private List<Vector3> posRecord = new List<Vector3>();
+    [SerializeField] GameObject GhostGO1;
+    [SerializeField] GameObject GhostGO2;
 
     [Header("Index(s)")]
     [SerializeField] bool isGhost1 = true;
+
+    private bool canMove = false;
+    private bool canMove2 = false;
+
+    private int moveIndexFor1 = 0;
+    private int moveIndexFor2 = 0;
     void Start()
     {
 
@@ -21,6 +29,16 @@ public class PosSave : MonoBehaviour
     private void FixedUpdate()
     {
         SavePos();
+        if (canMove)
+        {
+            GhostGO1.SetActive(true);
+            MoveGhost1();
+        }
+        if (canMove2)
+        {
+            GhostGO2.SetActive(true);
+            MoveGhost2();
+        }
     }
 
     public void SavePos()
@@ -47,9 +65,29 @@ public class PosSave : MonoBehaviour
         }
     }
 
-    public virtual void Move()
+    public void MoveGhost1()
     {
-
+        if (posForGhost1 != null)
+        {
+            GhostGO1.transform.position = posForGhost1[moveIndexFor1];
+            moveIndexFor1++;
+            if(moveIndexFor1 >= posForGhost1.Count)
+            {
+                moveIndexFor1 = 0;
+            }
+        }
+    }
+    public void MoveGhost2()
+    {
+        if (posForGhost2 != null)
+        {
+            GhostGO2.transform.position = posForGhost2[moveIndexFor2];
+            moveIndexFor2++;
+            if(moveIndexFor2 >= posForGhost2.Count)
+            {
+                moveIndexFor2 = 0;
+            }
+        }
     }
 
     public void CoppyPos()
@@ -58,11 +96,13 @@ public class PosSave : MonoBehaviour
         {
             posForGhost1 = new List<Vector3>(posRecord);
             posRecord.Clear();
+            canMove = true;
         }
         else
         {
             posForGhost2 = new List<Vector3>(posRecord);
             posRecord.Clear();
+            canMove2 = true;
         }
     }
 }
